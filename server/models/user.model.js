44b7d2836160ece7065cafcb132ed87c07478cc5
Mohammad Bakhshi <77542557+mohammad-bakhshi@users.noreply.github.com>
2022-07-29 19:22:07 +0430

    add authentication

---
+++ server/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: [true, "Username must be unique."],
+    },
+    passWord: {
+        type: String,
+        required: true
+    },
+});
+
+
+module.exports = mongoose.model('user', userSchema, 'users');
